ae629443c239b22d5a7a0549b9b8e41a75de797b
William Hursey <williamhursey1113@gmail.com>
2021-12-28 14:50:20 +0100

    do not init, expose Reveal API

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import Reveal from 'reveal.js'
 
-if (!window.Webflow) {
-  window.Webflow = []
-}
-window.Webflow.push(() => {
-  if (!window.Webflow.env('editor')) {
-    console.log('Initializing...')
-    Reveal.initialize()
-  } else {
-    const exitButton = document.createElement('button')
-    exitButton.innerText = 'Remove editor'
-    exitButton.classList.add('button-remove-editor')
-    exitButton.addEventListener('click', () => {
-      localStorage.removeItem('WebflowEditor')
-      location.reload()
+class WebflowReveal extends Reveal {
+  constructor (options = {}) {
+    super(options)
+
+    if (!window.Webflow) {
+      window.Webflow = []
+    }
+
+    window.Webflow.push(() => {
+      if (!window.Webflow.env('editor')) {
+        Reveal.initialize()
+      } else {
+        const exitButton = document.createElement('button')
+        exitButton.innerText = 'Remove editor'
+        exitButton.classList.add('button-remove-editor')
+        exitButton.addEventListener('click', () => {
+          localStorage.removeItem('WebflowEditor')
+          location.reload()
+        })
+        document.body.appendChild(exitButton)
+      }
     })
-    document.body.appendChild(exitButton)
   }
-})
+}
+
+window.Reveal = WebflowReveal
+
+export { WebflowReveal as Reveal }
